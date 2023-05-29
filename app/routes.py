2ffa5e14f8363da4237798a52215e72e0aaf0d42
Remy De la Cruz <remydlc@gmail.com>
2023-05-28 22:08:37 -0400

    Setting initial working site

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from app import app
+from flask import render_template
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    user = {"username": "Remy"}
+    return render_template("index.html", title="Home", user=user)
