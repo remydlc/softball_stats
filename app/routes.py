0983a61dd0e59d79641a40e70dfdcfba3edd9731
Remy De la Cruz <remydlc@Remys-MacBook-Pro.local>
2023-05-28 21:10:04 -0400

    Initial working min site

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,22 @@
-from app import app
 from flask import render_template
+from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
 @app.route("/index")
 def index():
     user = {"username": "Remy"}
-    return render_template("index.html", title="Home", user=user)
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+    ]
+    return render_template(
+        "index.html", title="Remy's Softball site", user=user, posts=posts
+    )
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign-in", form=form)
