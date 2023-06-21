c108055a4b48f8e2fcb71683f19672490136b3d4
Remy De la Cruz <remydlc@gmail.com>
2023-06-21 19:38:39 -0400

    Adding pagination

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    ADMINS = ["remydlc@amazon.com"]
-    POSTS_PER_PAGE = 10
+    ADMINS = ["remydlc@nine3one2.com"]
+    POSTS_PER_PAGE = 3
